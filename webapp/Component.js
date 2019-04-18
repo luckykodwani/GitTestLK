ff34a30e98063a80ab608f353389f9ae46552f44
Lucky Kodwani <lucky.kodwani27@gmail.com>
2019-04-18 16:09:31 +0000

    GIt_test_1

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZLK_MARA.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZLK_MARA.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
